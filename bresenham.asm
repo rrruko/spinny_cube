d1ef2e76471765475034295f72f4f98d8a3e92b5
rrruko <rukokarasu@gmail.com>
2018-06-18 03:20:21 +0000

    haha holy fuck

--- bresenham.asm
+++ bresenham.asm
@@ -1,19 +1,15 @@
 section .data
-	deltaX dd 0.0
-	deltaY dd 0.0
-	deltaErr dd 0.0
-	err dd 0.0
-	currY dd 0
-	deltaYSign dd 1
-	
-	startX dd 0
-	endX dd 0
-
-	half dd 0.5
-
-	plotCoords dd 0, 0
-	
 	buffer dd 0
+	startX dd 0
+	startY dd 0
+	endX dd 0
+	endY dd 0
+	diffX dd 0
+	diffY dd 0
+	sx dd 0
+	sy dd 0
+	err dd 0
+	e2 dd 0
 
 section .text
 global bresenham
@@ -22,144 +18,144 @@ global bresenham
 ; ecx points to a buffer
 bresenham:
 	pushad
-	;jmp .test_plot                ; Jump here to just plot endpoints
-
-	call .order_start_end
-	
 	mov [buffer], ecx
-
-	call .compute_deltaX
-	call .compute_deltaY
-	call .compute_deltaYSign
-	call .compute_deltaErr
-	call .init_currY
-	call .init_err
-	fwait
-
-	mov eax, [eax]
-	mov [startX], eax
-	mov ebx, [ebx]
-	mov [endX], ebx
-
-	mov ecx, [startX]              ; For x from x0 to x1
-	cmp ecx, [endX]
-	je .uhoh
-	.line_loop:
-		; plot (x,y)
-		mov [plotCoords],   ecx
-		mov ebx, [currY]
-		mov [plotCoords+4], ebx
-		mov eax, plotCoords
-		push ecx
-		mov ecx, [buffer]
-		call plot_point
-		pop ecx
-		
-		; error := error + deltaerr
-		fld dword [err]
-		fld dword [deltaErr]
-		faddp
-		fstp dword [err]
-		
-		.adjust_y: ; while error >= 0.5
-			fld dword [err]
-			fld dword [half]
-			fcomp
-			fstsw ax
-			fstp st0
-			sahf
-			fwait
-			jae .done_adjusting_y
-			; y := y + sign(deltaY) * 1
-			mov eax, [currY]
-			add eax, [deltaYSign]
-			mov [currY], eax
-
-			; error := error - 1.0
-			fld dword [err]
-			fld1
-			fsubp
-			fstp dword [err]
-		jmp .adjust_y
-	.done_adjusting_y:
-	inc ecx
-	cmp ecx, [endX]
-	jl .line_loop
-	popad
-	ret
-	
-
-.compute_deltaX:
-	fild dword [ebx]
-	fild dword [eax]
-	fsubp
-	fstp dword [deltaX]
-	ret
-.compute_deltaY:
-	fild dword [ebx+4]
-	fild dword [eax+4]
-	fsubp
-	fstp dword [deltaY]
-	ret
-.compute_deltaYSign:
-	fld dword [deltaY]
-	fld dword [deltaY]
-	fabs
-	fdivp
-	fistp dword [deltaYSign]
-	ret
-.compute_deltaErr:
-	fld dword [deltaY]
-	fld dword [deltaX]
-	fdivp
-	fabs
-	fstp dword [deltaErr]
-	ret
-.init_currY:
-	push eax
-	mov eax, [eax+4]
-	mov [currY], eax
-	pop eax
-	ret
-.init_err:
-	fldz
-	fstp dword [err]
-	ret
-
-.test_plot:
-	push ebx
-	call plot_point
-	pop ebx
-	mov eax, ebx
-	call plot_point
-	popad
-	ret
-
-.uhoh:
-	popad
-	ret
-
-; If necessary, swap the eax and ebx vectors so x_end is greater than x_start
-.order_start_end:
-	push ecx
+	mov ecx, [eax]
+	mov [startX], ecx
+	mov ecx, [eax+4]
+	mov [startY], ecx
 	mov ecx, [ebx]
-	sub ecx, [eax]
-	jl .swap
-	jmp .done
-	.swap:
-	xchg eax, ebx
-	.done:
-	pop ecx
+	mov [endX], ecx
+	mov ecx, [ebx+4]
+	mov [endY], ecx
+set_diffX_and_sx:
+	mov eax, [endX]
+	sub eax, [startX]
+	jg .positive_sx
+	.negative_sx:
+		mov ebx, -1
+		mov [sx], ebx
+		jmp .proceed
+	.positive_sx:
+		mov ebx, 1
+		mov [sx], ebx
+	.proceed:
+	call abs_val
+	mov [diffX], eax
+set_diffY_and_sy:
+	mov eax, [endY]
+	sub eax, [startY]
+	jg .positive_sy
+	.negative_sy:
+		mov ebx, -1
+		mov [sy], ebx
+	.positive_sy:
+		mov ebx, 1
+		mov [sy], ebx
+	.proceed:
+	call abs_val
+	mov [diffY], eax
+set_err:
+	mov eax, [diffX]
+	sub eax, [diffY]
+	jc .err_is_neg_diffY
+	.err_is_diffX:
+		mov eax, [diffX]
+		mov [err], eax
+		jmp .proceed
+	.err_is_neg_diffY:
+		xor eax, eax
+		sub eax, [diffY]
+		mov [err], eax
+	.proceed:
+	mov eax, [err]
+	mov ebx, 2
+	xor edx, edx
+	div ebx
+line_loop:
+	; setPixel(x0, y0)
+	mov eax, [startX]
+	mov ebx, [startY]
+	call set_pixel
+	;jmp .break
+	
+	; if (x0 == x1 && y0 == y1) break
+	mov eax, [startX]
+	sub eax, [endX]
+	mov ebx, [startY]
+	sub ebx, [endY]
+	or eax, ebx
+	cmp eax, 0
+	je .break
+
+	; wtf
+	mov eax, [startX]
+	cmp eax, 0
+	jl .break
+	cmp eax, 80
+	jg .break
+	mov eax, [startY]
+	cmp eax, 0
+	jl .break
+	cmp eax, 40
+	jg .break
+
+	; e2 = err
+	mov eax, [err]
+	mov [e2], eax
+
+	mov eax, [e2]
+	mov ebx, [diffX]
+	neg ebx
+	sub eax, ebx
+	jg .e2_gt_neg_diffX ; Do i need to return back here?
+	.e2_gt_neg_diffX_return:
+	mov eax, [e2]
+	sub eax, [diffY]
+	jl .e2_less_diffY ; Do i need to return back here?
+	.e2_less_diffY_return:
+	jmp line_loop
+
+	.e2_gt_neg_diffX:
+	mov eax, [err]
+	sub eax, [diffY]
+	mov [err], eax
+
+	mov eax, [startX]
+	add eax, [sx]
+	mov [startX], eax
+	jmp .e2_gt_neg_diffX_return
+
+	.e2_less_diffY:
+	mov eax, [err]
+	add eax, [diffX]
+	mov [err], eax
+
+	mov eax, [startY]
+	add eax, [sy]
+	mov [startY], eax
+	jmp .e2_less_diffY_return
+
+	.break:
+	popad
 	ret
 
-; plot 2d vector in eax to buffer in ecx
-plot_point:
-	push eax
-	mov eax, [eax+4]
-	mov ebx, dword 81
-	mul ebx
-	pop dword ebx
-	add eax, [ebx]
-	add eax, ecx
+; eax: x-coord, ebx: y-coord
+set_pixel:
+	pushad
+	xchg eax, ebx
+	mov ecx, dword 81
+	xor edx, edx
+	mul ecx
+	add eax, ebx
+	add eax, [buffer]
 	mov [eax], byte '@'
+	popad
+	ret
+
+abs_val:
+	cmp eax, 0
+	jl .is_negative
+	ret
+	.is_negative:
+	neg eax
 	ret
